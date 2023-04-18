3b511e69aa29caf9ed176dad1965f8bebe089eaa
raydermal <raydermal@gmail.com>
2023-04-17 22:50:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+
+    $('#CPF').mask('000.000.000-00', {
+        placeholder: "000.000.000-00"
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: "00000-00"
+    })
+
+ })
